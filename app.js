bd393656c36b902c227d27df78b5358ef1928b2b
maxime pyz <maxime pyz>
2016-04-04 21:15:35 +0200

    Integration de l'ordonnanceur (manque implémentation des objets dans les differents envois) + creation d un premier login

--- app.js
+++ app.js
@@ -24,6 +24,105 @@ app.use(cors());
 //     });
 // });
 
+//variable de jeu
+var playersMax = 3;
+var players = [];
+var etatJoueurs = [];
+var tabTimeout = [];
+var retour = null;
+var timeoutTime = 10000; //soit 10000 ms
+var plein = false;
+
+
+function initPartie(){
+	for(var i in players){
+		console.log(players[i]);
+	}
+	retour = players;
+}
+
+function updateTimeoutPlayer(index){
+	clearTimeout(tabTimeout[index]);
+	//Problème au moment de l'activation du timeout ...
+	//la fonction prend l'environnement courant ou lieu du moment où setTimeout est appelé
+	tabTimeout[index] = setTimeout(function() {
+		console.log(players[index]["pseudo"] + " timed out");
+		players.splice(index,1);
+		tabTimeout.splice(index,1);
+	}, timeoutTime);
+}
+
+function updateGame(){
+	etatJoueurs = [];
+}
+
+function jeuAJour(){
+	for(var i in players){
+		if(!etatJoueurs[i]){
+			return false;
+		}
+	}
+	return true;
+}
+
+
+function updatePlayers(joueur){
+	for(var i in players){
+		if(players[i]["pseudo"] == joueur["pseudo"]){
+			players[i] = joueur;
+			updateTimeoutPlayer(i);
+			etatJoueurs[i] = 1;
+			console.log("Mise à jour joueur : " + joueur["pseudo"]);
+		}
+	}
+}
+
+
+app.post("/game/joinGame", function(req,res){
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	if(/*players.length == playersMax ||*/ plein){
+		console.log("Partie pleine");
+    	res.end("plein");	
+	}
+	else{
+		for(var p in req.body){
+			var json = req.body[p];
+			players.push({
+				"pseudo":json
+			});
+			console.log("Un joueur vient de se connecter :" + json);
+		}
+		if(players.length == playersMax){
+			plein = true;
+			console.log("lancement de la partie !!");
+			initPartie();
+		}
+		res.end("bienvenue");
+	}
+});
+
+app.get("/game/getEtat", function(req,res){
+	res.setHeader('Content-Type', 'text/plain');
+	if(players.length == playersMax){
+		res.end("pret");
+	}
+	else{
+		res.end(players.length + "/" + playersMax);
+	}
+});
+
+app.post("/game/updateGame", function(req, res){
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	var json = req.body;
+	updatePlayers(json);
+	if(jeuAJour()){
+		console.log("Tous le monde a envoyé, mis à jour de la partie");
+		updateGame();
+		retour = players;
+	}
+	res.json(retour);
+});
+
 
 app.get("/server/getType", function(req, res){
 	res.setHeader('Content-Type', 'application/json');
@@ -38,10 +137,6 @@ app.get("/server/getType", function(req, res){
 		});
 });
 
-
-app.get("/server/setType", function(req, res){
-		
-});
 
 app.get("/server/getMap", function(req, res){
 	res.setHeader('Content-Type', 'application/json');
---
+++ js/main.js
@@ -0,0 +1,20 @@
+//Fichier Main (dans ce fichier on appelle différentes fonctions permettant de lancer le jeu)
+var donnees;
+var pseudo;
+
+$().ready(function(){
+	draw();
+	$("div.login input[type=button]").on("click", function(){
+        pseudo = $("input#name").val();
+        donnees = {
+            "pseudo":pseudo
+        }
+        console.log(donnees);
+		rejoindre(donnees, "div.login");
+	});
+	/*$(document).ajaxStop(function () {
+		//ajaxStop permet d'attendre la fin des différentes requêtes Ajax (chargement de map)
+      console.log("test");
+  	});*/
+	//Tout ce qui sera en dehors à partir d'ici sera lancé directement à l'ouverture de la page
+});
