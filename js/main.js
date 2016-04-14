1632be23610569d22927c2ab2c98c65add9d6615
Pyz Maxime <maxime.pyz@etu.umontpellier.fr>
2016-04-14 10:51:23 +0200

    Modification mode debug

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 //Fichier Main (dans ce fichier on appelle différentes fonctions permettant de lancer le jeu)
 var joueurP;
 var pseudo;
-var debug = 0;
+var debug = 1;
 
 //Création de personnage pour un test
 /*
@@ -39,6 +39,8 @@ $().ready(function(){
         joueurs[1].hexagone = 903;
         dessiner(joueurP);
         PositionnerImages();
+        var hexa = document.querySelector("[ordreHexagone='"+joueurP.hexagone+"'");
+        DeplacerPersonnage(hexa,1);
     }
 	/*$(document).ajaxStop(function () {
 		//ajaxStop permet d'attendre la fin des différentes requêtes Ajax (chargement de map)
