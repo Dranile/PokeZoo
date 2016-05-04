15b2f2535d09752823af7a41701969b70c73e711
Estreich Aymeric <aymeric.estreichetu.umontpellier.fr>
2016-05-04 12:59:51 +0200

    Test

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 //Fichier Main (dans ce fichier on appelle différentes fonctions permettant de lancer le jeu)
 var joueurP;
 var pseudo;
-var debug = 1;
+var debug = 0;
 
 //Création de personnage pour un test
 /*
